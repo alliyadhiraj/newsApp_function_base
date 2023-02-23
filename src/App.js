d7988a5685f66fc83a352c605a3fc653a5e73aa8
Alliya dhiraj <alliyadhirajsubhahchandani@gmail.com>
2023-02-23 21:55:00 +0530

    Stick navbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import './App.css';
 
-import React, { Component } from 'react'
-import { Navbar } from './component/Navbar';
+import React, { useState } from 'react'
+import Navbar  from './component/Navbar';
 import News from './component/News';
 import LoadingBar from 'react-top-loading-bar'
 
 
 import { Route, Routes } from 'react-router-dom';
 
-export default class App extends Component {
+const App = ()=>{
 
-  pageSize = 5;
+ const pageSize = 5;
 
-  state = {
-    progress: 0
-  }
-
-  setProgress = (progress)=>{
-    this.setState({
-      progress: progress
-    })
-  }
-
-  render() {
+   const [progress, setProgress] = useState(0)
+  
+    
     return (
       <>
         <Navbar />
@@ -30,21 +22,23 @@ export default class App extends Component {
         <LoadingBar
         height={3}
         color='#f11946'
-        progress={this.state.progress}
+        progress={progress}
         
       />
 
         <Routes>
-          <Route exact path="/" element={<News setProgress={this.setProgress} key="general" pageSize={5} country='in' category='general' />}></Route>
-          <Route exact path="/business" element={<News setProgress={this.setProgress} key="business" pageSize={5} country='in' category='business' />}></Route>
-          <Route exact path="/entertainment" element={<News setProgress={this.setProgress} key="entertainment" pageSize={5} country='in' category='entertainment' />}></Route>
-          <Route exact path="/general" element={<News setProgress={this.setProgress} key="general" pageSize={5} country='in' category='general' />}></Route>
-          <Route exact path="/health" element={<News setProgress={this.setProgress} key="health" pageSize={5} country='in' category='health' />}></Route>
-          <Route exact path="/science" element={<News setProgress={this.setProgress} key="science" pageSize={5} country='in' category='science' />}></Route>
-          <Route exact path="/sports" element={<News setProgress={this.setProgress} key="sports" pageSize={5} country='in' category='sports' />}></Route>
-          <Route exact path="/technology" element={<News setProgress={this.setProgress} key="tecnology" pageSize={5} country='in' category='technology' />}></Route>
+          <Route exact path="/" element={<News setProgress={setProgress} key="general" pageSize={5} country='in' category='general' />}></Route>
+          <Route exact path="/business" element={<News setProgress={setProgress} key="business" pageSize={5} country='in' category='business' />}></Route>
+          <Route exact path="/entertainment" element={<News setProgress={setProgress} key="entertainment" pageSize={5} country='in' category='entertainment' />}></Route>
+          <Route exact path="/general" element={<News setProgress={setProgress} key="general" pageSize={5} country='in' category='general' />}></Route>
+          <Route exact path="/health" element={<News setProgress={setProgress} key="health" pageSize={5} country='in' category='health' />}></Route>
+          <Route exact path="/science" element={<News setProgress={setProgress} key="science" pageSize={5} country='in' category='science' />}></Route>
+          <Route exact path="/sports" element={<News setProgress={setProgress} key="sports" pageSize={5} country='in' category='sports' />}></Route>
+          <Route exact path="/technology" element={<News setProgress={setProgress} key="tecnology" pageSize={5} country='in' category='technology' />}></Route>
         </Routes>
       </>
     )
-  }
+  
 }
+
+export default App;
--- src/component/Spinner.js
+++ src/component/Spinner.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Snake from './Snake.gif'
 
-export class spinner extends Component {
-  render() {
+const spinner =()=> {
+   
     return (
       <div className='text-center'>
             <img src={Snake} alt="Snake" />
       </div>
     )
-  }
+  
 }
 
 export default spinner
